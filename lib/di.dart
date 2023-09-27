d6932d16a0298592a6c68d5fc33381e6882efc97
shaymardanov9704 <shaymardanov9704@gmail.com>
2023-09-27 12:09:20 +0500

    add di

--- lib/di.dart
+++ lib/di.dart
@@ -1,21 +1,19 @@
 import 'dart:io';
-
 import 'package:easy_localization/easy_localization.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter/foundation.dart'
-    show defaultTargetPlatform, kIsWeb, TargetPlatform;
 import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:get_it/get_it.dart';
 import 'package:mini_chat/core/firebase/auth_service.dart';
+import 'package:mini_chat/core/firebase/firebase_service.dart';
 import 'package:mini_chat/core/firebase/firestore_service.dart';
+import 'package:mini_chat/core/firebase/storage_service.dart';
 import 'package:mini_chat/core/firebase/users_service.dart';
 import 'package:mini_chat/core/hive/cache_hive.dart';
 import 'package:mini_chat/core/hive/hive_base.dart';
 import 'package:mini_chat/core/hive/user_hive.dart';
-
-import 'core/firebase/storage_service.dart';
+import 'package:mini_chat/firebase_options.dart';
 
 final di = GetIt.instance;
 
@@ -56,8 +54,10 @@ Future<void> _setupConfigs() async {
 
 Future<void> _setupFactories() async {
   di.registerSingleton(HiveBase());
+
   await di.get<HiveBase>().init();
   di.registerSingleton(FirestoreService());
+  di.registerFactory(() => FirebaseService(di.get()));
   di.registerFactory(() => UsersService(
         di.get<FirestoreService>(),
         di.get(),
@@ -77,73 +77,3 @@ class _MyHttpOverrides extends HttpOverrides {
           (X509Certificate cert, String host, int port) => true;
   }
 }
-
-
-
-/// Default [FirebaseOptions] for use with your Firebase apps.
-///
-/// Example:
-/// ```dart
-/// import 'firebase_options.dart';
-/// // ...
-/// await Firebase.initializeApp(
-///   options: DefaultFirebaseOptions.currentPlatform,
-/// );
-/// ```
-class DefaultFirebaseOptions {
-  static FirebaseOptions get currentPlatform {
-    if (kIsWeb) {
-      throw UnsupportedError(
-        'DefaultFirebaseOptions have not been configured for web - '
-            'you can reconfigure this by running the FlutterFire CLI again.',
-      );
-    }
-    switch (defaultTargetPlatform) {
-      case TargetPlatform.android:
-        return android;
-      case TargetPlatform.iOS:
-        return ios;
-      case TargetPlatform.macOS:
-        throw UnsupportedError(
-          'DefaultFirebaseOptions have not been configured for macos - '
-              'you can reconfigure this by running the FlutterFire CLI again.',
-        );
-      case TargetPlatform.windows:
-        throw UnsupportedError(
-          'DefaultFirebaseOptions have not been configured for windows - '
-              'you can reconfigure this by running the FlutterFire CLI again.',
-        );
-      case TargetPlatform.linux:
-        throw UnsupportedError(
-          'DefaultFirebaseOptions have not been configured for linux - '
-              'you can reconfigure this by running the FlutterFire CLI again.',
-        );
-      default:
-        throw UnsupportedError(
-          'DefaultFirebaseOptions are not supported for this platform.',
-        );
-    }
-  }
-
-  static const FirebaseOptions android = FirebaseOptions(
-    apiKey: 'AIzaSyBytaD_roFj9ertV-wwIgS6P5aoCZ1tYxg',
-    appId: '1:41558850945:android:bab2c5a8bd83b0ca557c80',
-    messagingSenderId: '41558850945',
-    projectId: 'chateo-8fb31',
-    databaseURL: 'https://chateo-8fb31-default-rtdb.firebaseio.com',
-    storageBucket: 'chateo-8fb31.appspot.com',
-  );
-
-  static const FirebaseOptions ios = FirebaseOptions(
-    apiKey: 'AIzaSyCy2rGX6mGRj4aRV8qEe4xki65h-7Mk-ks',
-    appId: '1:41558850945:ios:031f2875c9503aef557c80',
-    messagingSenderId: '41558850945',
-    projectId: 'chateo-8fb31',
-    databaseURL: 'https://chateo-8fb31-default-rtdb.firebaseio.com',
-    storageBucket: 'chateo-8fb31.appspot.com',
-    androidClientId: '41558850945-eqe8atqondnoug8khfv5m56ejuh2ejrd.apps.googleusercontent.com',
-    iosClientId: '41558850945-vlflr1hp74fa1ovhiahtdbhh65rmhhkh.apps.googleusercontent.com',
-    iosBundleId: 'com.example.miniChat',
-  );
-}
-
--- lib/presentation/pages/users/bloc/users_bloc.dart
+++ lib/presentation/pages/users/bloc/users_bloc.dart
@@ -1,5 +1,6 @@
 import 'package:bloc/bloc.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:mini_chat/core/firebase/firebase_service.dart';
 import 'package:mini_chat/core/firebase/users_service.dart';
 import 'package:mini_chat/core/models/chat_user_model.dart';
 
@@ -10,9 +11,9 @@ part 'users_state.dart';
 part 'users_bloc.freezed.dart';
 
 class UsersBloc extends Bloc<UsersEvent, UsersState> {
-  final UsersService usersService;
+  final FirebaseService firebaseService;
 
-  UsersBloc(this.usersService) : super(UsersState.state()) {
+  UsersBloc(this.firebaseService) : super(UsersState.state()) {
     on<_init>(_emitInit);
   }
 
@@ -21,7 +22,7 @@ class UsersBloc extends Bloc<UsersEvent, UsersState> {
     Emitter<UsersState> emit,
   ) async {
     try {
-      final users = await usersService.fetchAllUsers();
+      final users = await firebaseService.fetchAllUsers();
       emit(state.copyWith(users: users, status: EnumStatus.success));
     } catch (e) {
       emit(state.copyWith(status: EnumStatus.fail));
